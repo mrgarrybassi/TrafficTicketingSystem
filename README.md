7a58cdbfde89f1dadd5170f9f3a4457c6d21ee6f
Garry Bassi <mrgarrybassi@gmail.com>
2020-11-23 01:33:33 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TrafficTicketingSystem
+ A dynamic website that allows traffic violators of City of XYZ to look at and pay notices online
---
+++ view/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>City of XYZ - Traffic Ticketing System</title>   
+    <link rel="stylesheet" type="text/css"
+          href="/TrafficTicketingSystem/main.css">
+    <div class="menu">
+      <ul>
+        <li><a href="#about">Staff Login</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+<h1>City of XYZ - Traffic Ticketing System</h1>
+<img class = "banner" src = "../images/banner.jpg">
+</head>
