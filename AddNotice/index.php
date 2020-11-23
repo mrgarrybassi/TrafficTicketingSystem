44a2497c69e14f7a11cd209cfc9aad40123fac3d
Garry Bassi <61808920+mrgarrybassi@users.noreply.github.com>
2020-11-23 14:16:58 -0800

    Create index.php

---
+++ AddNotice/index.php
@@ -0,0 +1,13 @@
+<?php include'../view/header.php'; ?>
+<body>
+     <h2>Please Login using your account.</h2>
+    <form METHOD="post">
+        <div class = "center">
+	        <input type="text" placeholder="email">
+          <input type="text" placeholder="password">
+        </div>
+        <div class = "center">
+          <button type="submit" name="violation_n" class="search_b">Search</button>
+        </div>
+    </form>
+<?php include'../view/footer.php'; ?>
