95fd908ec29c5430d568ac7b235f8cd3c76ec9c2
Garry Bassi <61808920+mrgarrybassi@users.noreply.github.com>
2020-11-23 17:56:51 -0800

    Update index.php

--- CreateNewNotice/index.php
+++ CreateNewNotice/index.php
@@ -1,10 +1,15 @@
 <?php include'../view/header.php'; ?>
 <body>
-     <h2>Please Login using your account.</h2>
+    <h2>Enter all the information.</h2>
     <form METHOD="post">
-        <div class = "center">
-	        <input type="text" placeholder="email">
-          <input type="text" placeholder="password">
+        <div>
+	  <input type="text" placeholder="Notice#">
+          <input type="text" placeholder="License Plate Number">
+	  <input type="text" placeholder="Driver's Name">
+	  <input type="text" placeholder="Driver's Address">
+	  <input type="text" placeholder="Violation Type">
+	  <input type="text" placeholder="Fine Amount">
+	  <input type="text" placeholder="Fine Due Date">
         </div>
         <div class = "center">
           <button type="submit" name="violation_n" class="search_b">Search</button>
