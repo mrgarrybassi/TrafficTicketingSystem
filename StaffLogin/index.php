8c0f59f2f4b688db434e3211660b68e2031716a8
Garry Bassi <61808920+mrgarrybassi@users.noreply.github.com>
2020-11-23 17:54:07 -0800

    Update index.php

--- StaffLogin/index.php
+++ StaffLogin/index.php
@@ -1,14 +1,15 @@
 <?php include'../view/header.php'; ?>
 <body>
-     <img src = "/TrafficTicketingSystem/images/avatar.png">
+     
      <h2>Please Login using your account.</h2>
-    <form METHOD="post">
-        <div class = "center">
+     <img src = "/TrafficTicketingSystem/images/avatar.png">
+    	<form METHOD="post">
+        	<div class = "center">
 	        <input type="text" placeholder="email">
-          <input type="text" placeholder="password">
+          	<input type="text" placeholder="password">
         </div>
         <div class = "center">
-          <button type="submit" name="violation_n" class="search_b">Search</button>
+          <button type="submit" name="violation_n" class="search_b">Login</button>
         </div>
     </form>
 <?php include'../view/footer.php'; ?>
